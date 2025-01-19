b142d9c1844229511e9f57d874159010c414fd8a
BugHunter-X <askzenai@gmail.com>
2025-01-19 04:12:08 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+function foo(a,b){return Number(a) + Number(b);}
+let x = foo(1,2);console.log(x);
+
+function foo2(a,b){return parseInt(a) + parseInt(b);}
+let y = foo2(1,2);console.log(y); 
+//Also you can use other type of parsing like parseFloat
